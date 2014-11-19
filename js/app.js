78e9e78e2fb7e35017944a7f674564dd114cb876
legomushroom <legomushroom@gmail.com>
2014-11-19 22:40:42 +0200

    working on templete resolving

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-window.App = Ember.Application.create();
+window.App = Ember.Application.create({
+  Resolver: Ember.DefaultResolver.extend({
+    resolveTemplate: function(parsedName) {
+      var resolvedTemplate;
+      console.log(parsedName.name);
+      resolvedTemplate = this._super(parsedName);
+      if (resolvedTemplate) {
+        return resolvedTemplate;
+      }
+      return Ember.TEMPLATES['not_found'];
+    }
+  })
+});
