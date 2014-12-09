f99a680b4a591096a16be6c2a69a06510ecfef2b
legomushroom <legomushroom@gmail.com>
2014-12-09 21:39:17 +0200

    main file refactor, add jade comple

--- .docit/docit.coffee
+++ .docit/docit.coffee
@@ -1,35 +1,39 @@
 jade = require("jade")
-watch = require("watch")
 gaze = require("gaze")
-concat = require("concat")
 
-pName = "docit"
-pagesFolder = "../" + pName + "-pages/"
-pagFiles = pagesFolder + "**/*.jade"
-splitFilePath = (p) ->
-  pathArr = p.split("/")
-  fileName = pathArr[pathArr.length - 1]
-  path = pathArr.slice(0, pathArr.length - 1)
-  pathStr = path.join("/") + "/"
-  path: pathStr
-  fileName: fileName
+class Main
+  constructor:(@o={})->
+    @vars()
+    @listenPages()
+  vars:->
+    @projectName = "docit"
+    @pagesFolder = "../#{@projectName}-pages"
+    @pagFiles = "#{@pagesFolder}/**/*.jade"
+  listenPages:->
+    it = @
+    gaze @pagFiles, (err, watcher) ->
+      # On file changed
+      @on 'changed', (filepath) ->
+        file = it.splitFilePath(filepath)
+        jade.renderFile(filepath)
+        console.log filepath + ' was changed'
+      # On file added
+      @on 'added', (filepath) ->
+        console.log filepath + ' was added'
+      # On file deleted
+      @on 'deleted', (filepath) ->
+        console.log filepath + ' was deleted'
+      @on 'all', (filepath) ->
+  splitFilePath:(p)->
+    pathArr = p.split("/")
+    fileName = pathArr[pathArr.length - 1]
+    path = pathArr.slice(0, pathArr.length - 1)
+    pathStr = path.join("/") + "/"
+    
+    file =
+      path: pathStr
+      fileName: fileName
 
-
-# Watch all .js files/dirs in process.cwd()
-gaze pagFiles, (err, watcher) ->
-  
-  # On file changed
-  @on "changed", (filepath) ->
-    file = splitFilePath(filepath)
-    console.log file
-    console.log filepath + " was changed"
-  
-  # On file added
-  @on "added", (filepath) ->
-    console.log filepath + " was added"
-  
-  # On file deleted
-  @on "deleted", (filepath) ->
-    console.log filepath + " was deleted"
+new Main
 
 console.log "it works"
--- .docit/docit.js
+++ .docit/docit.js
@@ -1,46 +1,60 @@
 #!/usr/bin/env node
 
-var concat, gaze, jade, pName, pagFiles, pagesFolder, splitFilePath, watch;
+var Main, gaze, jade;
 
 jade = require("jade");
 
-watch = require("watch");
-
 gaze = require("gaze");
 
-concat = require("concat");
+Main = (function() {
+  function Main(o) {
+    this.o = o != null ? o : {};
+    this.vars();
+    this.listenPages();
+  }
 
-pName = "docit";
-
-pagesFolder = "../" + pName + "-pages/";
-
-pagFiles = pagesFolder + "**/*.jade";
-
-splitFilePath = function(p) {
-  var fileName, path, pathArr, pathStr;
-  pathArr = p.split("/");
-  fileName = pathArr[pathArr.length - 1];
-  path = pathArr.slice(0, pathArr.length - 1);
-  pathStr = path.join("/") + "/";
-  return {
-    path: pathStr,
-    fileName: fileName
+  Main.prototype.vars = function() {
+    this.projectName = "docit";
+    this.pagesFolder = "../" + this.projectName + "-pages";
+    return this.pagFiles = "" + this.pagesFolder + "/**/*.jade";
   };
-};
 
-gaze(pagFiles, function(err, watcher) {
-  this.on("changed", function(filepath) {
-    var file;
-    file = splitFilePath(filepath);
-    console.log(file);
-    return console.log(filepath + " was changed");
-  });
-  this.on("added", function(filepath) {
-    return console.log(filepath + " was added");
-  });
-  return this.on("deleted", function(filepath) {
-    return console.log(filepath + " was deleted");
-  });
-});
+  Main.prototype.listenPages = function() {
+    var it;
+    it = this;
+    return gaze(this.pagFiles, function(err, watcher) {
+      this.on('changed', function(filepath) {
+        var file;
+        file = it.splitFilePath(filepath);
+        jade.renderFile(filepath);
+        return console.log(filepath + ' was changed');
+      });
+      this.on('added', function(filepath) {
+        return console.log(filepath + ' was added');
+      });
+      this.on('deleted', function(filepath) {
+        return console.log(filepath + ' was deleted');
+      });
+      return this.on('all', function(filepath) {});
+    });
+  };
+
+  Main.prototype.splitFilePath = function(p) {
+    var file, fileName, path, pathArr, pathStr;
+    pathArr = p.split("/");
+    fileName = pathArr[pathArr.length - 1];
+    path = pathArr.slice(0, pathArr.length - 1);
+    pathStr = path.join("/") + "/";
+    return file = {
+      path: pathStr,
+      fileName: fileName
+    };
+  };
+
+  return Main;
+
+})();
+
+new Main;
 
 console.log("it works");
