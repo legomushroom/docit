6a3bb73cf20fb93ea4d97260211c7a265a4d5c59
legomushroom <legomushroom@gmail.com>
2014-12-10 12:48:28 +0200

    add map generation

--- .docit/docit.coffee
+++ .docit/docit.coffee
@@ -1,5 +1,6 @@
 jade = require("jade")
 gaze = require("gaze")
+jf   = require('jsonfile')
 
 class Main
   constructor:(@o={})->
@@ -9,30 +10,58 @@ class Main
     @projectName = "docit"
     @pagesFolder = "../#{@projectName}-pages"
     @pagFiles = "#{@pagesFolder}/**/*.jade"
+    @compilePage = @compilePage.bind @
+    @removePage  = @removePage.bind @
+    @generateJSONMap = @generateJSONMap.bind @
   listenPages:->
     it = @
     gaze @pagFiles, (err, watcher) ->
-      # On file changed
-      @on 'changed', (filepath) ->
-        file = it.splitFilePath(filepath)
-        jade.renderFile(filepath)
-        console.log filepath + ' was changed'
-      # On file added
-      @on 'added', (filepath) ->
-        console.log filepath + ' was added'
-      # On file deleted
-      @on 'deleted', (filepath) ->
-        console.log filepath + ' was deleted'
-      @on 'all', (filepath) ->
+      @relative it.generateJSONMap
+      @on 'changed', it.compilePage
+      @on 'added',   it.compilePage
+      @on 'deleted', it.removePage
+  
+  compilePage:(filepath)->
+    file = @splitFilePath(filepath)
+    if !file.path.match /\/partials\//
+      jade.renderFile(filepath)
+
+  removePage:(filepath)->
+
+
+  generateJSONMap:(err, files)->
+    map = {}
+    Object.keys(files).forEach (key)=>
+      return if key is "#{@pagesFolder}/partials/"
+      folder = @getFolder key
+      if folder is "#{@projectName}-pages" then folder = 'pages'
+      items = []
+      files[key].forEach (item)=>
+        if !@isFolder item then items.push item.replace('.jade', '')
+      map[folder] = items
+
+    jf.writeFile 'pages.json', map, (err)->
+      if err then console.error 'could not write to pages.json'
+      
+  isFolder:(path)-> path.substr(path.length-1) is '/'
+  getFolder:(path)->
+    pathArr = path.split '/'
+    folder = pathArr[pathArr.length-2]
+    folder
+
+
   splitFilePath:(p)->
     pathArr = p.split("/")
     fileName = pathArr[pathArr.length - 1]
+    extension = fileName.split('.')
+    extension = extension[extension.length-1]
     path = pathArr.slice(0, pathArr.length - 1)
     pathStr = path.join("/") + "/"
     
     file =
-      path: pathStr
-      fileName: fileName
+      path:       pathStr
+      fileName:   fileName
+      extension:  extension
 
 new Main
 
--- .docit/docit.js
+++ .docit/docit.js
@@ -1,10 +1,10 @@
-#!/usr/bin/env node
-
-var Main, gaze, jade;
+var Main, gaze, jade, jf;
 
 jade = require("jade");
 
 gaze = require("gaze");
+
+jf = require('jsonfile');
 
 Main = (function() {
   function Main(o) {
@@ -16,38 +16,85 @@ Main = (function() {
   Main.prototype.vars = function() {
     this.projectName = "docit";
     this.pagesFolder = "../" + this.projectName + "-pages";
-    return this.pagFiles = "" + this.pagesFolder + "/**/*.jade";
+    this.pagFiles = "" + this.pagesFolder + "/**/*.jade";
+    this.compilePage = this.compilePage.bind(this);
+    this.removePage = this.removePage.bind(this);
+    return this.generateJSONMap = this.generateJSONMap.bind(this);
   };
 
   Main.prototype.listenPages = function() {
     var it;
     it = this;
     return gaze(this.pagFiles, function(err, watcher) {
-      this.on('changed', function(filepath) {
-        var file;
-        file = it.splitFilePath(filepath);
-        jade.renderFile(filepath);
-        return console.log(filepath + ' was changed');
-      });
-      this.on('added', function(filepath) {
-        return console.log(filepath + ' was added');
-      });
-      this.on('deleted', function(filepath) {
-        return console.log(filepath + ' was deleted');
-      });
-      return this.on('all', function(filepath) {});
+      this.relative(it.generateJSONMap);
+      this.on('changed', it.compilePage);
+      this.on('added', it.compilePage);
+      return this.on('deleted', it.removePage);
     });
   };
 
+  Main.prototype.compilePage = function(filepath) {
+    var file;
+    file = this.splitFilePath(filepath);
+    if (!file.path.match(/\/partials\//)) {
+      return jade.renderFile(filepath);
+    }
+  };
+
+  Main.prototype.removePage = function(filepath) {};
+
+  Main.prototype.generateJSONMap = function(err, files) {
+    var map;
+    map = {};
+    Object.keys(files).forEach((function(_this) {
+      return function(key) {
+        var folder, items;
+        if (key === ("" + _this.pagesFolder + "/partials/")) {
+          return;
+        }
+        folder = _this.getFolder(key);
+        if (folder === ("" + _this.projectName + "-pages")) {
+          folder = 'pages';
+        }
+        items = [];
+        files[key].forEach(function(item) {
+          if (!_this.isFolder(item)) {
+            return items.push(item.replace('.jade', ''));
+          }
+        });
+        return map[folder] = items;
+      };
+    })(this));
+    return jf.writeFile('pages.json', map, function(err) {
+      if (err) {
+        return console.error('could not write to pages.json');
+      }
+    });
+  };
+
+  Main.prototype.isFolder = function(path) {
+    return path.substr(path.length - 1) === '/';
+  };
+
+  Main.prototype.getFolder = function(path) {
+    var folder, pathArr;
+    pathArr = path.split('/');
+    folder = pathArr[pathArr.length - 2];
+    return folder;
+  };
+
   Main.prototype.splitFilePath = function(p) {
-    var file, fileName, path, pathArr, pathStr;
+    var extension, file, fileName, path, pathArr, pathStr;
     pathArr = p.split("/");
     fileName = pathArr[pathArr.length - 1];
+    extension = fileName.split('.');
+    extension = extension[extension.length - 1];
     path = pathArr.slice(0, pathArr.length - 1);
     pathStr = path.join("/") + "/";
     return file = {
       path: pathStr,
-      fileName: fileName
+      fileName: fileName,
+      extension: extension
     };
   };
 
